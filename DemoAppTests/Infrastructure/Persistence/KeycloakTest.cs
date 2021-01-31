4b2fb43470fd6363e83810bd152b17438818ab72
Dominik Guhr <d.guhr@slashwhy.de>
2021-01-31 21:19:18 +0100

    removed unused class (cleanup)

--- DemoAppTests/Infrastructure/Persistence/KeycloakTest.cs
+++ DemoAppTests/Infrastructure/Persistence/KeycloakTest.cs
@@ -88,31 +88,4 @@ namespace DemoAppTests.Infrastructure.Persistence
             await KeycloakContainer.DisposeAsync();
         }
     }
-
-    class TokenContainer
-    {
-        [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
-        
-        [JsonProperty("expires_in")]
-        public int ExpiresIn { get; set; }
-        
-        [JsonProperty("refresh_expires_in")]
-        public int RefreshExpiresIn { get; set; }
-        
-        [JsonProperty("refresh_token")]
-        public string RefreshToken { get; set; }
-        
-        [JsonProperty("token_type")]
-        public string TokenType { get; set; }
-        
-        [JsonProperty("not-before-policy")]
-        public int NotBeforePolicy { get; set; }
-        
-        [JsonProperty("session_state")]
-        public string SessionState { get; set; }
-        
-        [JsonProperty("scope")]
-        public string Scope { get; set; }
-    }
 }
