f6ce2142bd69e79ca09149f2355ab5f644213af6
Dominik Guhr <d.guhr@slashwhy.de>
2021-02-04 12:36:14 +0100

    WIP - Not working HTTPS (Http would work)

---
+++ DemoApp/Bootstrap/Jwt/JwtConfig.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+
+namespace DemoApp.Bootstrap.Jwt
+{
+    public static class JwtConfiguration
+    {
+        public static void ConfigureJwtAuthentication(this IServiceCollection services,
+            Action<JwtBearerOptions> configureOptions)
+        {
+            services.AddAuthentication(options => { options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; })
+                .AddJwtBearer(options =>
+                {
+                    ConfigureDefaultJwtAuthentication(options);
+                    configureOptions.Invoke(options);
+                });
+        }
+
+        private static void ConfigureDefaultJwtAuthentication(JwtBearerOptions options)
+        {
+            options.RequireHttpsMetadata = false;
+            options.IncludeErrorDetails = true;
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateLifetime = true
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnTokenValidated = context =>
+                {
+                    MapKeycloakRolesToRoleClaims(context);
+                    return Task.CompletedTask;
+                }
+            };
+        }
+
+        public static void ConfigureJwtAuthorization(this IServiceCollection services)
+        {
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser().Build());
+            });
+        }
+
+        private static void MapKeycloakRolesToRoleClaims(TokenValidatedContext context)
+        {
+            
+            var resourceAccess = JObject.Parse(context.Principal.FindFirst("resource_access").Value);
+            var clientResource = resourceAccess[context.Principal.FindFirstValue("aud")];
+            var clientRoles = clientResource["roles"];
+            var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+            if (claimsIdentity == null)
+            {
+                return;
+            }
+
+            foreach (var clientRole in clientRoles)
+            {
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, clientRole.ToString()));
+            }
+        }
+    }
+}
--- DemoAppTests/Api/ArticlesControllerTest.cs
+++ DemoAppTests/Api/ArticlesControllerTest.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using DemoApp.Bootstrap;
 using Microsoft.AspNetCore.Hosting;
@@ -11,19 +12,27 @@ namespace DemoAppTests.Api
 {
     public class ArticlesControllerTest
     {
+        private readonly Dictionary<string, string> _testConfig = new Dictionary<string, string>
+        {
+            {
+                "DB:ConnectionString",
+                "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=demo-db;Pooling=true;"
+            },
+            {
+                "Jwt:Audience",
+                "account"
+            },
+            {
+                "Jwt:Issuer",
+                "https://" + BaseFixture.KeycloakContainer.Hostname + ":8443/auth/realms/example"
+            }
+        };
+
         [Test]
         public async Task SucceedsWhenGetRequestReturnsListOfArticles()
         {
-            var inMemorySettings = new Dictionary<string, string>
-            {
-                {
-                    "DB:ConnectionString",
-                    "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=demo-db;Pooling=true;"
-                }
-            };
-
             IConfiguration configuration = new ConfigurationBuilder()
-                .AddInMemoryCollection(inMemorySettings)
+                .AddInMemoryCollection(_testConfig)
                 .Build();
 
             var webHostBuilder = new WebHostBuilder().UseConfiguration(configuration).UseStartup<Startup>();
@@ -37,14 +46,58 @@ namespace DemoAppTests.Api
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
-        /*[Test]
-        public async Task SucceedsWhenCallToProtectedResourceWithoutTokenReturns401()
+        [Test]
+        public async Task SucceedsWhenGetRequestOnProtectedResourceReturnsUnauthorizedWithoutToken()
         {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(_testConfig)
+                .Build();
+
+            var webHostBuilder = new WebHostBuilder().UseConfiguration(configuration).UseStartup<Startup>();
+            using var server = new TestServer(webHostBuilder);
+            using var client = server.CreateClient();
+            const string url = "https://localhost:5001/api/Articles/Protected";
+
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
         [Test]
-        public async Task SucceedsWhenCallToProtectedResourceWithTokenReturnsArticles()
+        public async Task SucceedsWhenGetRequestWithTokenReturnsListOfArticles()
         {
-        }*/
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(_testConfig)
+                .Build();
+
+            var webHostBuilder = new WebHostBuilder().UseConfiguration(configuration).UseEnvironment("Testing")
+                .UseStartup<Startup>();
+            //not working because TestClient does not use this HttpClient (or to be precise: JwtMiddleware does not use it)
+            /*.ConfigureTestServices(services => 
+                    services.AddHttpClient("TestClient", httpClient =>
+                    {
+                    }).ConfigurePrimaryHttpMessageHandler(() =>
+                    {
+                        var handler = new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                        };
+                        return handler;
+                    }));*/
+
+            using var server = new TestServer(webHostBuilder);
+            using var client = server.CreateClient();
+
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", BaseFixture.TestToken);
+
+            const string url = "https://localhost:5001/api/Articles/Protected";
+
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
     }
 }
